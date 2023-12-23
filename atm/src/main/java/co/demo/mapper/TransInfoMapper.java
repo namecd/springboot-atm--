bab617d3b1ff429744dc3c1c83b586ce9d7749d5
namecd <2824869720@qq.com>
2023-12-23 14:32:47 +0800

    后端新增查询账单功能

--- atm/src/main/java/co/demo/mapper/TransInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package co.demo.mapper;
-
-import co.demo.domain.TransInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface TransInfoMapper extends Mapper<TransInfo> {
-}
--- atm/src/main/java/co/demo/mapper/UserInfoMapper.java
+++
@@ -1,7 +0,0 @@
-package co.demo.mapper;
-
-import co.demo.domain.UserInfo;
-import tk.mybatis.mapper.common.Mapper;
-
-public interface UserInfoMapper extends Mapper<UserInfo> {
-}
